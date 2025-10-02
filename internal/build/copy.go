b950ddd77171f628a862d5530e55b3be7428925d
ftechmax <29160411+ftechmax@users.noreply.github.com>
2025-10-02 08:45:16 +0200

    add prune logic during source sync (#10)

--- internal/build/copy.go
+++ internal/build/copy.go
@@ -60,7 +60,13 @@ func disablePortforwarding(pfProcess *os.Process) {
 }
 
 func copySource(kubeConfig string, projectName string, skipWeb bool, password string) (bool, error) {
-	filesCopied := 0
+	startTime := time.Now()
+	filesAdded := 0
+	filesUpdated := 0
+	filesDeleted := 0
+
+	plusSym := utils.Colorize("+", utils.Green)
+	minusSym := utils.Colorize("-", utils.Red)
 
 	fmt.Printf("Copying project %s to remote server\n", projectName)
 
@@ -111,6 +117,7 @@ func copySource(kubeConfig string, projectName string, skipWeb bool, password st
 	
 	remotePath := filepath.ToSlash(filepath.Join(sftpWorkspacePath, projectName))
 	serviceDir := filepath.ToSlash(filepath.Join(config.KrunSourceConfig.Path, projectName))
+	localEntries := make(map[string]struct{})
 
 	err = filepath.Walk(serviceDir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
@@ -137,7 +144,12 @@ func copySource(kubeConfig string, projectName string, skipWeb bool, password st
 				fmt.Println("Failed to get relative path:", err)
 				return err
 			}
+			
 			remoteDir := filepath.ToSlash(filepath.Join(remotePath, relPath))
+			if relPath != "." && relPath != "" {
+				localEntries[filepath.ToSlash(relPath)] = struct{}{}
+			}
+
 			err = sftpClient.MkdirAll(remoteDir)
 			if err != nil {
 				fmt.Println("Failed to create remote directory:", err)
@@ -160,30 +172,35 @@ func copySource(kubeConfig string, projectName string, skipWeb bool, password st
 			}
 
 			remoteFile := filepath.ToSlash(filepath.Join(remotePath, relPath))
+			localEntries[filepath.ToSlash(relPath)] = struct{}{}
 
 			// Check if remote file exists and is older
-			remoteInfo, err := sftpClient.Stat(remoteFile)
-			if err == nil {
+			remoteInfo, statErr := sftpClient.Stat(remoteFile)
+			if statErr == nil {
 				if !info.ModTime().After(remoteInfo.ModTime()) {
 					return nil
 				}
 			}
 
-			fmt.Println("Copying file:", path)
-			filesCopied++
+			if statErr != nil { // file does not exist remotely
+				filesAdded++
+			} else {
+				filesUpdated++
+			}
+			fmt.Printf("%s %s\n", plusSym, filepath.ToSlash(relPath))
 
 			// Create remote file (Linux path)
 			dstFile, err := sftpClient.Create(remoteFile)
 			if err != nil {
 				return fmt.Errorf("Failed to create remote file: %w", err)
 			}
-			defer dstFile.Close()
 
 			// Copy local file to remote file
-			_, err = io.Copy(dstFile, srcFile)
-			if err != nil {
+			if _, err = io.Copy(dstFile, srcFile); err != nil {
 				return fmt.Errorf("Failed to copy file: %w", err)
 			}
+			_ = dstFile.Close()
+			_ = srcFile.Close()
 		}
 		return nil
 	})
@@ -192,9 +209,68 @@ func copySource(kubeConfig string, projectName string, skipWeb bool, password st
 		return false, fmt.Errorf("Error walking the path: %w", err)
 	}
 
-	if filesCopied > 0 {
-		return true, nil
-	} else {
+	// Walk remote tree
+	w := sftpClient.Walk(remotePath)
+	var remoteDirs []string
+	for w.Step() {
+		if w.Err() != nil {
+			continue
+		}
+		
+		p := w.Path()
+		if p == remotePath { // skip root
+			continue
+		}
+		
+		rel := strings.TrimPrefix(p, remotePath+"/")
+		if rel == "" {
+			continue
+		}
+		
+		rel = filepath.ToSlash(rel)
+		if _, exists := localEntries[rel]; !exists {
+			fi := w.Stat()
+			if fi.IsDir() {
+				remoteDirs = append(remoteDirs, p)
+			} else {
+				if err := sftpClient.Remove(p); err == nil {
+					fmt.Printf("%s %s\n", minusSym, strings.TrimPrefix(p+"/", remotePath+"/"))
+					filesDeleted++
+				}
+			}
+		}
+	}
+
+	// Remove directories in reverse order to ensure leaf dirs are removed first
+	for i := len(remoteDirs) - 1; i >= 0; i-- {
+		p := remoteDirs[i]
+		rel := strings.TrimPrefix(p, remotePath+"/")
+		if _, exists := localEntries[filepath.ToSlash(rel)]; !exists {
+			if err := sftpClient.RemoveDirectory(p); err == nil {
+				fmt.Printf("%s %s/\n", minusSym, strings.TrimPrefix(p, remotePath+"/"))
+				filesDeleted++
+			}
+		}
+	}
+
+	elapsed := time.Since(startTime)
+	shortDur := elapsed.Truncate(10 * time.Millisecond)
+	if filesAdded == 0 && filesUpdated == 0 && filesDeleted == 0 {
+		fmt.Printf("%s | %s %s\n",
+			utils.Colorize("No changes - project up to date", utils.Gray),
+			utils.Colorize("Completed sync in", utils.Gray),
+			utils.Colorize(shortDur.String(), utils.Gray),
+		)
 		return false, nil
 	}
+
+	addedStr := utils.Colorize(fmt.Sprintf("Added: %d", filesAdded), utils.Green)
+	updatedStr := utils.Colorize(fmt.Sprintf("Updated: %d", filesUpdated), utils.Yellow)
+	deletedStr := utils.Colorize(fmt.Sprintf("Deleted: %d", filesDeleted), utils.Red)
+	timeLabel := utils.Colorize("Completed sync in", utils.Gray)
+	timeVal := utils.Colorize(shortDur.String(), utils.Gray)
+	
+	fmt.Printf("Sync summary: %s  %s  %s  | %s %s\n", addedStr, updatedStr, deletedStr, timeLabel, timeVal)
+	
+	return true, nil
 }
